bb265eab2b512478fdc290ecfc41763debcb0128
cool9203 <octer18@gmail.com>
2024-09-09 16:16:47 +0800

    Add: `get_function_used_params`

--- fun_workflow/core/util.py
+++ fun_workflow/core/util.py
@@ -1,1 +1,29 @@
 # coding: utf-8
+
+from inspect import signature
+from typing import Any, Callable, Dict
+
+__all__ = ("get_function_used_params",)
+
+
+def get_function_used_params(
+    callable: Callable,
+    **kwds: Dict,
+) -> Dict[str, Any]:
+    """Get `callable` need parameters from kwds.
+
+    Args:
+        callable (Callable): function
+
+    Returns:
+        Dict[str, Any]: parameters
+    """
+    parameters = dict()
+    callable_parameters = signature(callable).parameters
+    for parameter, value in kwds.items():
+        if parameter in callable_parameters:
+            if value:
+                parameters.update({parameter: value})
+            else:
+                parameters.update({parameter: None})
+    return parameters
